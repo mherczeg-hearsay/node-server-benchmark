9f789a318bfd2d8ef020d8ffab2e9d9df557a983
mherczeg-hearsay <mherczeg@hearsaycorp.com>
2018-07-10 13:59:23 +0200

    added complex test to handlebars

--- templates/handlebars/renderer.js
+++ templates/handlebars/renderer.js
@@ -1,6 +1,8 @@
 import handlebars from 'handlebars';
 import * as template from './template.html';
 
+handlebars.registerHelper('times', (times, block) => Array(times).fill('').map((item, index) => block.fn(index)).join(''));
+
 export const renderer = (data) => {
     const render = handlebars.compile(template);
     return render(data);
